bc1bb2f25f3dd4a98e392a31b8a10a252795a024
Loki701 <figueredoj189@gmail.com>
2023-11-26 19:29:55 -0500

    final update

--- tracing.js
+++ tracing.js
@@ -1,9 +1,10 @@
 const { Resource } = require("@opentelemetry/resources");
 const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { trace } = require("@opentelemetry/api");
+// ... other imports remain the same
 
 //Instrumentations
 const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
@@ -11,16 +12,21 @@ const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongo
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
 
-//Exporter
 module.exports = (serviceName) => {
-   const exporter = new ConsoleSpanExporter();
+   const jaegerExporter = new JaegerExporter({
+       serviceName: serviceName,
+       endpoint: 'http://localhost:14268/api/traces', // Jaeger endpoint
+   });
+
    const provider = new NodeTracerProvider({
        resource: new Resource({
            [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
        }),
    });
-   provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+   
+   provider.addSpanProcessor(new SimpleSpanProcessor(jaegerExporter));
    provider.register();
+   
    registerInstrumentations({
        instrumentations: [
            new HttpInstrumentation(),
@@ -29,5 +35,6 @@ module.exports = (serviceName) => {
        ],
        tracerProvider: provider,
    });
+   
    return trace.getTracer(serviceName);
 };
